64386dab83695c9e01ff30af6af964c71baefcc4
Yuri Moysés <yuri09.moyses@gmail.com>
2023-11-16 21:40:41 -0300

    Add novos metodos

--- JwtAspNet/Program.cs
+++ JwtAspNet/Program.cs
@@ -1,19 +1,45 @@
+using JwtAspNet;
 using JwtAspNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<TokenService>();
 
-builder.Services.AddAuthentication();
-builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(x => 
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x => 
+    {
+        x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(x => 
+{
+    x.AddPolicy("Admin", p => p.RequireRole("admin"));
+});
 
 var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/", (TokenService service) => 
+app.MapGet("/login", (TokenService service) => 
 {
     return service.CreateToken(new JwtAspNet.Models.User(1, "Yuri Moyses", "yuri@gmail.com", "https://yuri.io/", "1234", new[] { "student", "admin" }));
-}).RequireAuthorization();
+});
+
+app.MapGet("/restrito", () => "Voce tem acesso")
+    .RequireAuthorization();
+
+app.MapGet("/admin", () => "Voce tem acesso")
+    .RequireAuthorization("admin");
 
 app.Run();
--- JwtAspNet/Services/TokenService.cs
+++ JwtAspNet/Services/TokenService.cs
@@ -10,55 +10,55 @@ public class TokenService
 {
     public string CreateToken(User user)
     {
-		try
-		{
-			var handler = new JwtSecurityTokenHandler();
+        try
+        {
+            var handler = new JwtSecurityTokenHandler();
 
-			var key = Encoding.ASCII.GetBytes(Configuration.PrivateKey);
+            var key = Encoding.ASCII.GetBytes(Configuration.PrivateKey);
 
-			var credential = new SigningCredentials
-			(
-				new SymmetricSecurityKey(key),
-				algorithm: SecurityAlgorithms.HmacSha256
-			);
+            var credential = new SigningCredentials
+            (
+                new SymmetricSecurityKey(key),
+                algorithm: SecurityAlgorithms.HmacSha256
+            );
 
-			var tokenDescriptor = new SecurityTokenDescriptor 
-			{
-				SigningCredentials = credential,
-				Expires = DateTime.UtcNow.AddMinutes(30),
-				Subject = GenerateClaims(user)
-			};
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = credential,
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                Subject = GenerateClaims(user)
+            };
 
-			var token = handler.CreateToken(tokenDescriptor);
+            var token = handler.CreateToken(tokenDescriptor);
 
-			return handler.WriteToken(token);
-		}
-		catch (Exception)
-		{
-			throw;
-		}
+            return handler.WriteToken(token);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
     }
 
-	private static ClaimsIdentity GenerateClaims (User user) 
-	{
-		try
-		{
-			var ci = new ClaimsIdentity();
+    private static ClaimsIdentity GenerateClaims(User user)
+    {
+        try
+        {
+            var ci = new ClaimsIdentity();
 
-			ci.AddClaim(new Claim("id", user.Id.ToString()));
-			ci.AddClaim(new Claim(ClaimTypes.Name, user.Email));
-			ci.AddClaim(new Claim(ClaimTypes.Name, user.Name));
-			ci.AddClaim(new Claim(ClaimTypes.GivenName, user.Name));
-			ci.AddClaim(new Claim("image", user.Image));
+            ci.AddClaim(new Claim("id", user.Id.ToString()));
+            ci.AddClaim(new Claim(ClaimTypes.Name, user.Email));
+            ci.AddClaim(new Claim(ClaimTypes.Name, user.Name));
+            ci.AddClaim(new Claim(ClaimTypes.GivenName, user.Name));
+            ci.AddClaim(new Claim("image", user.Image));
 
-			foreach (string role in user.Roles) 
-				ci.AddClaim(new Claim(ClaimTypes.Role, role));
+            foreach (string role in user.Roles)
+                ci.AddClaim(new Claim(ClaimTypes.Role, role));
 
-			return ci;
-		}
-		catch (Exception)
-		{
-			throw;
-		}
-	}
+            return ci;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 }
